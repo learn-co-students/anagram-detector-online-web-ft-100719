aacae70183038f1278d892e349f673a91e0bc44d
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-14 17:42:42 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(word_array)
+    anagrams = []
+    sorted_letters = @word.split("").sort
+    word_array.each do |words|
+      anagrams << words if words.split("").sort == sorted_letters
+    end
+    return anagrams
+  end
+  
+end
