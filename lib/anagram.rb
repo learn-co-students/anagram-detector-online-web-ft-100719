6bda85ac356ee7a6ede879013541da157a344df6
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-18 04:15:49 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+class Anagram
+attr_accessor :word
+
+  def initialize(word)
+    @word=word
+  end
+  
+  def match(list)
+    answer=[]
+    list.each do |mix|
+      if @word.split("").sort==mix.split("").sort
+        answer<< mix
+      end
+    end
+    answer
+  end
+  
+  
+end
