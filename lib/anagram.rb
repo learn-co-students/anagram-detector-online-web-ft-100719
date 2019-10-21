35285b7661bbef539ba1b2a2d5f44c93e64b10f8
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-10-21 02:22:35 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+attr_accessor :word
+
+ANAGRAMS=[ ]
+
+  def initialize(word)
+    @word=word
+  end
+
+  def match(array)
+    @array=array
+    @array.find_all{|poss_ana|poss_ana.split("").sort==@word.split("").sort}
+
+
+  end
+
+end
