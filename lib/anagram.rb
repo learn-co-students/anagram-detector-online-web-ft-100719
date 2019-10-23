c3080a2ff745a3aac95f1e85166d4812917e49f8
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-23 14:58:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
-# Your code goes here!
+class Anagram# Your code goes here!
+attr_accessor :match
+@@ll = []
+def initialize (match)
+  @match = match
+ end 
+end 
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana))
+
