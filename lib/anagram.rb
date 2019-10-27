6248b290b3979ba895df54e233aac587fd0a91b2
Prince Charles <princecharles35@yahoo.com>
2019-10-27 16:59:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  def initailize(word)
+    @word = word
+    
+  end 
+  
+  def match(array_of_word)
+    array_of_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+    end
+  end
+  
+  
+end
+
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana))
+ 
